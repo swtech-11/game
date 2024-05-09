dfc7ebca934395ee18028d25d49ca8e60fbeae80
softwhale <softwhale@icloud.com>
2024-05-09 07:50:22 -0300

    Debug structure: Pan Camera, Persistent window location, framerate

---
+++ src/config.rs
@@ -0,0 +1,2 @@
+pub const CELL_SIZE: f32 = 10.0;
+pub const GRID_SIZE: u8 = 3;
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,36 @@
+mod config;
+mod custom_debug;
 use bevy::prelude::*;
+use bevy_framepace::FramepacePlugin;
+use bevy_pancam::{PanCam, PanCamPlugin};
+use custom_debug::DebugPlugin;
 
 fn main() {
-    App::new().run();
+    App::new()
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: None,
+                ..default()
+            }),
+            DebugPlugin,
+            FramepacePlugin,
+            PanCamPlugin::default(),
+        ))
+        .init_resource::<Game>()
+        .add_systems(Startup, setup)
+        .run();
+}
+
+#[derive(Resource, Default)]
+struct Game {
+    entities: Vec<Entity>,
+}
+
+fn setup(mut commands: Commands, mut settings: ResMut<bevy_framepace::FramepaceSettings>) {
+    use bevy_framepace::Limiter;
+    settings.limiter = Limiter::from_framerate(60.0);
+
+    commands
+        .spawn(Camera2dBundle::default())
+        .insert(PanCam::default());
 }
