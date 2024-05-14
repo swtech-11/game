8b427371f098d09482eb0627f8434fca1983e325
softwhale <softwhale@icloud.com>
2024-05-14 02:49:38 -0300

    Add manual control for debugging without window

--- src/game_logic/mod.rs
+++ src/game_logic/mod.rs
@@ -22,7 +22,10 @@ pub struct GameLogicPlugin;
 impl Plugin for GameLogicPlugin {
     fn build(&self, app: &mut App) {
         info!("Initializing GameLogicPlugin");
+        #[cfg(feature = "render")]
         app.add_systems(Update, move_creature);
+        #[cfg(not(feature = "render"))]
+        app.add_systems(Update, move_single_creature);
     }
 }
 
@@ -67,19 +70,41 @@ fn move_creature(
     for mut position in query.iter_mut() {
         if input.pressed(KeyCode::ArrowRight) && position.x <= GRID_SIZE - 1 {
             position.x += 1;
-            println!("Creature new location x: {}, y: {}", position.x, position.y);
         }
         if input.pressed(KeyCode::ArrowUp) && position.y <= GRID_SIZE - 1 {
             position.y += 1;
-            println!("Creature new location x: {}, y: {}", position.x, position.y);
         }
         if input.pressed(KeyCode::ArrowLeft) && position.x > 0 {
             position.x -= 1;
-            println!("Creature new location x: {}, y: {}", position.x, position.y);
         }
         if input.pressed(KeyCode::ArrowDown) && position.y > 0 {
             position.y -= 1;
-            println!("Creature new location x: {}, y: {}", position.x, position.y);
         }
+        println!("Creature new location x: {}, y: {}", position.x, position.y);
+    }
+}
+
+fn move_single_creature(mut query: Query<&mut Position, With<Creature>>) {
+    for mut position in query.iter_mut() {
+        use std::io::{self, Write};
+        let mut input_string = String::new();
+        print!("Move with wasd: ");
+        io::stdout().flush().unwrap(); // Make sure the printed text appears before input
+        io::stdin().read_line(&mut input_string).unwrap();
+        input_string = input_string.trim().to_string();
+
+        if input_string == "d" && position.x <= GRID_SIZE - 1 {
+            position.x += 1;
+        }
+        if input_string == "w" && position.y <= GRID_SIZE - 1 {
+            position.y += 1;
+        }
+        if input_string == "a" && position.x > 0 {
+            position.x -= 1;
+        }
+        if input_string == "s" && position.y > 0 {
+            position.y -= 1;
+        }
+        println!("Creature new location x: {}, y: {}", position.x, position.y);
     }
 }
