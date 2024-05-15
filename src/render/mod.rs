d486db99fc04e3c932567334555675fcaae08a08
softwhale <softwhale@icloud.com>
2024-05-15 09:24:01 -0300

    Create render for creatures

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,22 +1,8 @@
-use crate::game_logic::Position;
+use crate::game_logic::{Creature, Position};
 use log;
 mod debug;
 
-// MaterialMesh2dBundle {
-//     mesh,
-//     material,
-//     transform: Transform::from_scale(Vec3 {
-//         x: (10.0),
-//         y: (10.0),
-//         z: (0.0),
-//     }),
-//     ..default()
-// },
-
-// meshes.add(Rectangle::default()).into(),
-//         materials.add(Color::PURPLE),
-
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 use bevy_pancam::{PanCam, PanCamPlugin};
 
 pub const CELL_SIZE: f32 = 10.0;
@@ -27,15 +13,16 @@ impl Plugin for RenderPlugin {
     fn build(&self, app: &mut App) {
         log::info!("Initializing RenderPlugin");
 
-        app.add_plugins((PanCamPlugin::default(),))
-            .add_systems(Startup, setup)
-            .add_systems(Update, position_sync);
-
         #[cfg(feature = "debug")]
         {
             use debug::RenderDebugPlugin;
             app.add_plugins(RenderDebugPlugin);
         }
+
+        app.add_plugins((PanCamPlugin::default(),))
+            .add_systems(Startup, setup)
+            .add_systems(PostStartup, create_render)
+            .add_systems(Update, position_sync);
     }
 }
 
@@ -43,6 +30,29 @@ fn setup(mut commands: Commands) {
     commands
         .spawn(Camera2dBundle::default())
         .insert(PanCam::default());
+}
+
+fn create_render(
+    mut commands: Commands,
+    entity: Query<Entity, With<Creature>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let mesh_bundle = MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        material: materials.add(Color::PURPLE),
+        transform: Transform::from_scale(Vec3 {
+            x: (10.0),
+            y: (10.0),
+            z: (0.0),
+        }),
+        ..default()
+    };
+
+    for entity in entity.iter() {
+        dbg!(entity);
+        commands.entity(entity).insert(mesh_bundle.clone());
+    }
 }
 
 fn position_sync(mut query: Query<(&Position, &mut Transform), Changed<Position>>) {
