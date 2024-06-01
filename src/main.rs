3427ff89d39f649a910cb770272d8b9f085a167d
softwhale <softwhale@icloud.com>
2024-06-01 16:19:44 -0300

    Add persistent window plugin

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 use camera::CameraPlugin;
+use persistent_window::PersistentWindowPlugin;
 
 mod camera;
+mod persistent_window;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: None,
+                    ..Default::default()
+                })
+                .build(),
+        )
+        .add_plugins(PersistentWindowPlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_plugins(RapierDebugRenderPlugin::default())
         .add_systems(Startup, setup_physics)
-        .add_systems(Update, print_ball_altitude)
         .run();
 }
 
@@ -27,10 +36,4 @@ fn setup_physics(mut commands: Commands) {
         .insert(Collider::ball(50.0))
         .insert(Restitution::coefficient(0.7))
         .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
-}
-
-fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
-    for transform in positions.iter() {
-        println!("Ball altitude: {}", transform.translation.y);
-    }
 }
---
+++ src/persistent_window.rs
@@ -0,0 +1,35 @@
+use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_persistent::prelude::*;
+use bevy_persistent_windows::prelude::*;
+
+pub struct PersistentWindowPlugin;
+
+impl Plugin for PersistentWindowPlugin {
+    fn build(&self, app: &mut App) {
+        let state_directory = dirs::data_dir()
+            .expect("failed to get the platforms data directory")
+            .join("your-amazing-game")
+            .join("state");
+
+        app.world.spawn((
+            PrimaryWindow,
+            PersistentWindowBundle {
+                window: Window {
+                    title: "I am the primary window!".to_owned(),
+                    ..Default::default()
+                },
+                state: Persistent::<WindowState>::builder()
+                    .name("primary window state")
+                    .format(StorageFormat::Toml)
+                    .path(state_directory.join("primary-window.toml"))
+                    .default(WindowState::windowed(1280, 720))
+                    .revertible(true)
+                    .revert_to_default_on_deserialization_errors(true)
+                    .build()
+                    .expect("failed to create the persistent primary window state"),
+            },
+        ));
+
+        app.add_plugins(PersistentWindowsPlugin);
+    }
+}
