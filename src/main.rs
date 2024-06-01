e53f0d18e97674ae1e1788e903cea2dce019eae1
softwhale <softwhale@icloud.com>
2024-06-01 15:35:55 -0300

    New structure with rapier2d

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,41 @@
-mod game_logic;
-mod logger;
-mod render;
-
-use bevy::{log::LogPlugin, prelude::*};
-
-use game_logic::GameLogicPlugin;
+use bevy::prelude::*;
+use bevy_pancam::{PanCam, PanCamPlugin};
+use bevy_rapier2d::prelude::*;
 
 fn main() {
-    logger::init();
+    App::new()
+        .add_plugins((DefaultPlugins, PanCamPlugin::default()))
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugins(RapierDebugRenderPlugin::default())
+        .add_systems(Startup, setup_graphics)
+        .add_systems(Startup, setup_physics)
+        .add_systems(Update, print_ball_altitude)
+        .run();
+}
 
-    let mut app = App::new();
-    app.add_plugins(GameLogicPlugin);
+fn setup_graphics(mut commands: Commands) {
+    // Add a camera so we can see the debug-render.
+    commands
+        .spawn(Camera2dBundle::default())
+        .insert(PanCam::default());
+}
 
-    #[cfg(feature = "render")]
-    {
-        use render::RenderPlugin;
-        app.add_plugins(DefaultPlugins.build().disable::<LogPlugin>());
-        app.add_plugins(RenderPlugin);
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
+}
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
     }
-    #[cfg(not(feature = "render"))]
-    {
-        use bevy::input::InputPlugin;
-        app.add_plugins((MinimalPlugins, InputPlugin));
-    }
-
-    app.run();
 }
