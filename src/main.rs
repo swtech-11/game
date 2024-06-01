3fe42907a7d12f7105cad86e0c20835b541e3b8a
softwhale <softwhale@icloud.com>
2024-06-01 18:44:45 -0300

    chore: Add bevy-inspector-egui dependency and plugins for creature and world inspector

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use camera::CameraPlugin;
+use creature::CreaturePlugin;
 use persistent_window::PersistentWindowPlugin;
 use physics::PhysicsPlugin;
 
 mod camera;
+mod creature;
 mod persistent_window;
 mod physics;
 
@@ -20,5 +23,7 @@ fn main() {
         .add_plugins(PersistentWindowPlugin)
         .add_plugins(CameraPlugin)
         .add_plugins(PhysicsPlugin)
+        .add_plugins(CreaturePlugin)
+        .add_plugins(WorldInspectorPlugin::new())
         .run();
 }
