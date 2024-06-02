61fd5aa3dff7c1ac5fa7fbbc9bf094b6841cf583
softwhale <softwhale@icloud.com>
2024-06-02 13:18:13 -0300

    Better gizmos

--- src/gizmos.rs
+++ src/gizmos.rs
@@ -4,21 +4,32 @@ pub struct GizmosPlugin;
 
 impl Plugin for GizmosPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, draw_gizmos);
+        app.add_systems(Startup, setup)
+            .add_systems(Update, draw_gizmos);
     }
 }
 
-fn draw_gizmos(mut commands: Commands, mut gizmos: Gizmos) {
+fn setup(mut config_store: ResMut<GizmoConfigStore>) {
+    let (config, _) = config_store.config_mut::<DefaultGizmoConfigGroup>();
+    config.line_width = 1.0;
+}
+
+fn draw_gizmos(mut gizmos: Gizmos) {
     let arbitrary_size = 1000;
     let start = arbitrary_size as f32;
     let end = -arbitrary_size as f32;
     for i in -arbitrary_size..arbitrary_size {
         let mut color = Color::DARK_GRAY;
-        if i % 5 == 0 {
+        if i == 0 {
+            color = Color::WHITE;
+        } else if i % 5 == 0 {
             color = Color::GRAY;
         }
-        gizmos.line_2d(Vec2::new(i as f32, end), Vec2::new(i as f32, start), color);
 
-        gizmos.line_2d(Vec2::new(start, i as f32), Vec2::new(end, i as f32), color);
+        let j = i as f32;
+
+        gizmos.line_2d(Vec2::new(j, end), Vec2::new(j, start), color);
+
+        gizmos.line_2d(Vec2::new(start, j), Vec2::new(end, j), color);
     }
 }
