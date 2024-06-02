d0e5aae29f8c87d43b16450aaa98f7d1a25e2b46
softwhale <softwhale@icloud.com>
2024-06-02 20:29:52 -0300

    Add drag forces

--- src/physics.rs
+++ src/physics.rs
@@ -6,20 +6,13 @@ pub struct PhysicsPlugin;
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(0.0))
-            .add_plugins(RapierDebugRenderPlugin::default());
+            .add_plugins(RapierDebugRenderPlugin::default())
+            .add_systems(Update, drag);
     }
 }
 
-// fn setup_physics(mut commands: Commands) {
-//     /* Create the ground. */
-//     commands
-//         .spawn(Collider::cuboid(500.0, 50.0))
-//         .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
-
-//     /* Create the bouncing ball. */
-//     commands
-//         .spawn(RigidBody::Dynamic)
-//         .insert(Collider::ball(50.0))
-//         .insert(Restitution::coefficient(0.7))
-//         .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
-// }
+fn drag(mut query: Query<&mut Velocity>) {
+    for mut velocity in &mut query {
+        velocity.linvel *= 0.99;
+    }
+}
