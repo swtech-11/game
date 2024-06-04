a78cc4b45605ed07394e211a9fa76bac608ee88a
softwhale <softwhale@icloud.com>
2024-06-03 22:41:08 -0300

    chore: Move rendering related code to separate module

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,9 @@ use bevy::prelude::*;
 use fps::FPSPlugin;
 use game_logic::GameLogicPlugin;
 
-mod camera;
 mod fps;
 mod game_logic;
-mod gizmos;
-mod persistent_window;
+mod render;
 
 fn main() {
     let mut app = App::new();
@@ -17,24 +15,7 @@ fn main() {
 
     #[cfg(feature = "render")]
     {
-        use bevy::time::TimePlugin;
-        use bevy_inspector_egui::quick::WorldInspectorPlugin;
-        use camera::CameraPlugin;
-        use gizmos::GizmosPlugin;
-        use persistent_window::PersistentWindowPlugin;
-
-        let default_plugins = DefaultPlugins
-            .build()
-            .disable::<WindowPlugin>()
-            .disable::<TaskPoolPlugin>()
-            .disable::<TypeRegistrationPlugin>()
-            .disable::<FrameCountPlugin>()
-            .disable::<TimePlugin>();
-        app.add_plugins(default_plugins)
-            .add_plugins(PersistentWindowPlugin)
-            .add_plugins(CameraPlugin)
-            .add_plugins(GizmosPlugin)
-            .add_plugins(WorldInspectorPlugin::new());
+        app.add_plugins(render::RenderPlugin);
     }
 
     app.run()
---
+++ src/render/mod.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+use bevy::time::TimePlugin;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use camera::CameraPlugin;
+use gizmos::GizmosPlugin;
+use persistent_window::PersistentWindowPlugin;
+
+mod camera;
+mod gizmos;
+mod persistent_window;
+
+pub struct RenderPlugin;
+
+impl Plugin for RenderPlugin {
+    fn build(&self, app: &mut App) {
+        let default_plugins = DefaultPlugins
+            .build()
+            .disable::<WindowPlugin>()
+            .disable::<TaskPoolPlugin>()
+            .disable::<TypeRegistrationPlugin>()
+            .disable::<FrameCountPlugin>()
+            .disable::<TimePlugin>();
+        app.add_plugins(default_plugins)
+            .add_plugins(PersistentWindowPlugin)
+            .add_plugins(CameraPlugin)
+            .add_plugins(GizmosPlugin)
+            .add_plugins(WorldInspectorPlugin::new());
+    }
+}
