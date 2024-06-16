273257cd7de9b0c19bb246e8669f4f384a2b5495
softwhale <softwhale@icloud.com>
2024-06-16 03:05:34 -0300

    Basic physics engine to fixed timestep

--- src/physics_engine.rs
+++ src/physics_engine.rs
@@ -1,10 +1,21 @@
 use bevy::prelude::*;
-use bevy_rapier2d::prelude::*;
+use bevy_rapier2d::plugin::{NoUserData, RapierConfiguration, RapierPhysicsPlugin, TimestepMode};
 
 pub struct PhysicsEnginePlugin;
 
 impl Plugin for PhysicsEnginePlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(0.0));
+        app.add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+            .insert_resource(RapierConfiguration {
+                gravity: Vec2::ZERO,
+                physics_pipeline_active: true,
+                query_pipeline_active: true,
+                timestep_mode: TimestepMode::Fixed {
+                    dt: 1.0 / 60.0,
+                    substeps: 1,
+                },
+                scaled_shape_subdivision: 10,
+                force_update_from_transform_changes: false,
+            });
     }
 }
