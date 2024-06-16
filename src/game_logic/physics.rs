9087ea6168fb71c0e1b1a9ba20a32ae1ca32840c
softwhale <softwhale@icloud.com>
2024-06-16 03:05:42 -0300

    Remove drag for now

--- src/game_logic/physics.rs
+++ src/game_logic/physics.rs
@@ -5,12 +5,12 @@ pub struct PhysicsPlugin;
 
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, drag);
+        // app.add_systems(Update, drag);
     }
 }
 
-fn drag(mut query: Query<&mut Velocity>) {
-    for mut velocity in &mut query {
-        velocity.linvel *= 0.99;
-    }
-}
+// fn drag(mut query: Query<&mut Velocity>) {
+//     for mut velocity in &mut query {
+//         velocity.linvel *= 0.99;
+//     }
+// }
