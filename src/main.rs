100f997d4e4c3674ec7ac967ffb9297d34a8af04
softwhale <softwhale@icloud.com>
2024-06-16 06:25:24 -0300

    refactor: Update main.rs to use separate app instances for rendering and non-rendering
    This commit refactors the `main.rs` file to use separate app instances for rendering and non-rendering scenarios. It introduces conditional compilation using the `cfg` attribute to determine which app instance to create. The code related to rendering is enclosed in a `cfg(feature = "render")` block, while the code related to non-rendering is enclosed in a `cfg(not(feature = "render"))` block. This change improves code organization and allows for better control over the app's behavior based on the presence or absence of the render feature.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-#![allow(unused_imports)]
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 use game::{
-    app_with_render, app_without_render,
     config::ConfigRes,
     game_logic::entities::{
         creature::{Creature, CreatureBundle, Nutrition},
         fruit::{Fruit, FruitBundle},
     },
-    render::controllable::Controllable,
 };
 
 fn main() {
+    let mut app: App;
     #[cfg(feature = "render")]
-    let mut app = game::app_with_render();
+    {
+        app = game::app_with_render();
+        debug!("App with render");
+    }
     #[cfg(not(feature = "render"))]
-    let mut app = game::app_without_render();
+    {
+        app = game::app_without_render();
+        debug!("App without render");
+    }
 
     app.insert_resource(ConfigRes {
         bounds: (Vec2::new(-20.0, -20.0), Vec2::new(20.0, 20.0)),
@@ -33,17 +37,17 @@ fn main() {
         },
         velocity: Velocity::default(),
         impulse: ExternalImpulse {
-            impulse: Vec2::new(1.0, 0.0),
+            impulse: Vec2::new(3.0, 0.0),
             ..Default::default()
         },
     });
     app.world.spawn(FruitBundle {
         fruit: Fruit,
-        collider: Collider::cuboid(1.0, 1.0),
+        collider: Collider::cuboid(0.5, 0.5),
         rigid_body: RigidBody::Dynamic,
         active_events: ActiveEvents::COLLISION_EVENTS,
         transform: TransformBundle {
-            local: Transform::from_xyz(10.0, 10.0, 0.0),
+            local: Transform::from_xyz(3.0, 0.0, 0.0),
             ..Default::default()
         },
     });
