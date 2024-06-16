e172cb3906a277ca8ac35c4d3d80fc5c097dc6a3
softwhale <softwhale@icloud.com>
2024-06-15 23:12:52 -0300

    Remve non render for now

--- src/lib.rs
+++ src/lib.rs
@@ -12,22 +12,13 @@ pub mod render;
 mod rng;
 mod state;
 
-fn app() -> App {
+pub fn app() -> App {
     let mut app = App::new();
     app.add_plugins(MinimalPlugins)
         .add_plugins(GameLogicPlugin)
         .add_plugins(CustomLogPlugin)
         .add_plugins(StatePlugin)
-        .add_plugins(PhysicsEnginePlugin);
+        .add_plugins(PhysicsEnginePlugin)
+        .add_plugins(render::RenderPlugin);
     app
-}
-
-pub fn app_with_render() -> App {
-    let mut app = app();
-    app.add_plugins(render::RenderPlugin);
-    app
-}
-
-pub fn app_without_render() -> App {
-    app()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 use bevy::prelude::*;
 use bevy_rapier2d::prelude::*;
 use game::{
-    app_with_render, app_without_render,
     config::ConfigRes,
     game_logic::entities::{
         creature::{Creature, CreatureBundle, Nutrition},
@@ -44,16 +43,7 @@ fn _fruit_creature_run() {
     };
 
     let mut app;
-    #[cfg(feature = "render")]
-    {
-        log::debug!("Game with render");
-        app = game::app_with_render();
-    }
-    #[cfg(not(feature = "render"))]
-    {
-        log::debug!("Game without render");
-        app = game::app_without_render();
-    }
+    app = game::app();
 
     let creature_entity = app.world.spawn(creature).id();
     app.world.spawn(fruit.clone());
@@ -62,91 +52,8 @@ fn _fruit_creature_run() {
     let mut creature_ref = app.world.get_entity_mut(creature_entity).unwrap();
     {
         let mut impulse = creature_ref.get_mut::<ExternalImpulse>().unwrap();
-        impulse.impulse = Vec2::new(10.0, 0.0);
+        impulse.impulse = Vec2::new(1.0, 0.0);
     }
 
     app.run();
-}
-
-fn _fruit_creature_steps() {
-    let creature = CreatureBundle {
-        creature: Creature,
-        nutrition: Nutrition(0),
-        collider: Collider::ball(1.0),
-        rigid_body: RigidBody::Dynamic,
-        active_events: ActiveEvents::COLLISION_EVENTS,
-        transform: TransformBundle {
-            local: Transform::from_xyz(3.0, 3.0, 0.0),
-            ..Default::default()
-        },
-        impulse: ExternalImpulse::default(),
-    };
-    let fruit = FruitBundle {
-        fruit: Fruit,
-        collider: Collider::cuboid(1.0, 1.0),
-        rigid_body: RigidBody::Dynamic,
-        active_events: ActiveEvents::COLLISION_EVENTS,
-        transform: TransformBundle {
-            local: Transform::from_xyz(10.0, 10.0, 0.0),
-            ..Default::default()
-        },
-    };
-    let config = ConfigRes {
-        bounds: Vec2::new(20.0, 20.0),
-    };
-
-    let mut app;
-    #[cfg(feature = "render")]
-    {
-        app = game::app_with_render();
-        panic!("You cannot render when doing step by step")
-    }
-    #[cfg(not(feature = "render"))]
-    {
-        app = game::app_without_render();
-    }
-
-    let creature_entity = app.world.spawn(creature).id();
-    app.world.spawn(fruit);
-    app.insert_resource(config);
-
-    app.update();
-
-    {
-        let creature_ref = app.world.get_entity(creature_entity).unwrap();
-        let transform = creature_ref.get::<Transform>().unwrap();
-        dbg!(transform.translation);
-    }
-    app.update();
-
-    {
-        let mut creature_ref = app.world.get_entity_mut(creature_entity).unwrap();
-        {
-            let mut impulse = creature_ref.get_mut::<ExternalImpulse>().unwrap();
-            impulse.impulse = Vec2::new(10.0, 0.0);
-        }
-        let transform = creature_ref.get::<Transform>().unwrap();
-        dbg!(transform.translation);
-    }
-    app.update();
-
-    {
-        let creature_ref = app.world.get_entity(creature_entity).unwrap();
-        let transform = creature_ref.get::<Transform>().unwrap();
-        dbg!(transform.translation);
-    }
-    app.update();
-
-    {
-        let creature_ref = app.world.get_entity(creature_entity).unwrap();
-        let transform = creature_ref.get::<Transform>().unwrap();
-        dbg!(transform.translation);
-    }
-    app.update();
-
-    {
-        let creature_ref = app.world.get_entity(creature_entity).unwrap();
-        let transform = creature_ref.get::<Transform>().unwrap();
-        dbg!(transform.translation);
-    }
 }
